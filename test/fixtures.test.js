db49c902b8a8f42e4d7ff65badd8d23e7061e0f1
rease <cftcview@gmail.com>
2022-06-26 03:46:50 +0500

    add more fixtures

--- test/fixtures.test.js
+++ test/fixtures.test.js
@@ -5,37 +5,12 @@ const path = require('path')
 
 const DIR_FIXTURES = path.resolve(__dirname, 'fixtures')
 
-test('VUE:', () => {
-  const vue = fs.readFileSync(
-    path.resolve(DIR_FIXTURES, 'vue/vue.runtime.common.dev.min.js'), 'utf8'
-  )
-  const tokens = jsx2tokens(vue, { useJSX: false })
-  expect(tokens[tokens.length - 1].deep).toEqual(0)
-})
-
-test('THREE:', () => {
-  let tokens
-  const three_js = fs.readFileSync(
-    path.resolve(DIR_FIXTURES, 'three/three.js'), 'utf8'
-  )
-  tokens = jsx2tokens(three_js, { useJSX: false })
-  expect(tokens[tokens.length - 1].deep).toEqual(0)
-
-  const three_cjs = fs.readFileSync(
-    path.resolve(DIR_FIXTURES, 'three/three.cjs'), 'utf8'
-  )
-  tokens = jsx2tokens(three_cjs, { useJSX: false })
-  expect(tokens[tokens.length - 1].deep).toEqual(0)
-
-  const three_module_js = fs.readFileSync(
-    path.resolve(DIR_FIXTURES, 'three/three.module.js'), 'utf8'
-  )
-  tokens = jsx2tokens(three_module_js, { useJSX: false })
-  expect(tokens[tokens.length - 1].deep).toEqual(0)
-
-  const three_min_js = fs.readFileSync(
-    path.resolve(DIR_FIXTURES, 'three/three.min.js'), 'utf8'
-  )
-  tokens = jsx2tokens(three_min_js, { useJSX: false })
-  expect(tokens[tokens.length - 1].deep).toEqual(0)
+fs.readdirSync(DIR_FIXTURES).forEach((dir) => {
+  fs.readdirSync(path.resolve(DIR_FIXTURES, dir)).forEach((file) => {
+    test('FIXTURES: ' + dir + ' - ' + file, () => {
+      const code = fs.readFileSync(path.resolve(DIR_FIXTURES, dir, file), 'utf8')
+      const tokens = jsx2tokens(code, { useJSX: false })
+      expect(tokens[tokens.length - 1].deep).toEqual(0)
+    })
+  })
 })
