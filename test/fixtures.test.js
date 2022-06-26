6dc2c6b9a3b40709aad57d5510ba24bd719a21f5
rease <cftcview@gmail.com>
2022-06-26 05:15:50 +0500

    add test fixtures

--- test/fixtures.test.js
+++ test/fixtures.test.js
@@ -5,12 +5,19 @@ const path = require('path')
 
 const DIR_FIXTURES = path.resolve(__dirname, 'fixtures')
 
-fs.readdirSync(DIR_FIXTURES).forEach((dir) => {
-  fs.readdirSync(path.resolve(DIR_FIXTURES, dir)).forEach((file) => {
-    test('FIXTURES: ' + dir + ' - ' + file, () => {
-      const code = fs.readFileSync(path.resolve(DIR_FIXTURES, dir, file), 'utf8')
-      const tokens = jsx2tokens(code, { useJSX: false })
-      expect(tokens[tokens.length - 1].deep).toEqual(0)
-    })
+const testFilesFromDir = (dir) => {
+  fs.readdirSync(dir).forEach((file) => {
+    const full = path.resolve(dir, file)
+    if (fs.statSync(full).isDirectory() && !/test/.test(file)) {
+      testFilesFromDir(full)
+    } else if (/\.[jt]sx?$/.test(file)) {
+      test(`FIXTURES (${dir}): ${file}`, () => {
+        const code = fs.readFileSync(full, 'utf8')
+        const tokens = jsx2tokens(code, { useJSX: /x$/.test(file) })
+        expect(tokens[tokens.length - 1].deep).toEqual(0)
+      })
+    }
   })
-})
+}
+
+testFilesFromDir(DIR_FIXTURES)
